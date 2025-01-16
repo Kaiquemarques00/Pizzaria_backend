eddd4547cf6c0cb83c33e5e5e21554a79587406c
Kaique marques <kaiquemarques581@outlook.com>
2025-01-16 14:11:02 -0300

    Adicionando funcionalidade de listagem das categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,11 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailsUserController } from "./controllers/user/DetailsUserController";
+
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
@@ -15,5 +18,6 @@ router.get('/me', isAuthenticated, new DetailsUserController().handle)
 
 /* ROTAS CATEGORY */
 router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+router.get('/categories', isAuthenticated, new ListCategoryController().handle);
 
 export { router };
